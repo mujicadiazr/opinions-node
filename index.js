fc35650829fc982f11cca98a26547c0c4c137725
Randy Mujica Diaz <mujicadiazr@gmail.com>
2017-03-07 21:26:40 -0500

    Changing to my node app

--- index.js
+++ index.js
@@ -1,20 +1,80 @@
-var express = require('express');
-var app = express();
+var io = require('socket.io').listen(8008);
 
-app.set('port', (process.env.PORT || 5000));
+var querystring = require('querystring');
+var http = require('http');
 
-app.use(express.static(__dirname + '/public'));
+io.sockets.on('connection', function (socket) {
+    socket.on('vote_up',function (info) {
+        var values = querystring.stringify(info);
+        
+        //VIA POST
+        // var options = {
+        //     hostname: 'localhost',
+        //     port: '8000',
+        //     path: '/plus/',
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'x-www-form-urlencoded',
+        //         'Content-Length': values.length
+        //     }
+        // }
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+        //VIA GET
+        var options = {
+            hostname: 'localhost',
+            port: '8000',
+            path: '/plus/' + info.enlace_id,
+            method: 'GET'
+        }
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+        var req = http.request(options, function (res) {
+           console.log('Vino de vuelta');
+           res.setEncoding('utf8');
+           res.on('data', function (data) {
+               console.log('Emitiendo: '+data);
+               io.sockets.emit('vote_updated', data); 
+           }); 
+        });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+        req.write(values);
+        req.end();
+    });
+
+    socket.on('vote_down',function (info) {
+        var values = querystring.stringify(info);
+        
+        //VIA POST
+        // var options = {
+        //     hostname: 'localhost',
+        //     port: '8000',
+        //     path: '/plus/',
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'x-www-form-urlencoded',
+        //         'Content-Length': values.length
+        //     }
+        // }
+
+        //VIA GET
+        var options = {
+            hostname: 'localhost',
+            port: '8000',
+            path: '/minus/' + info.enlace_id,
+            method: 'GET'
+        }
+
+        var req = http.request(options, function (res) {
+           console.log('Vino de vuelta');
+           res.setEncoding('utf8');
+           res.on('data', function (data) {
+               console.log('Emitiendo: '+data);
+               io.sockets.emit('vote_updated', data); 
+           }); 
+        });
+
+        req.write(values);
+        req.end();
+    });
 });
 
 
